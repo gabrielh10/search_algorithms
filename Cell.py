368fcc968b0d18e5216e6eb42112ad8a62e05eff
ghds <ghds@cin.ufpe.br>
2020-07-21 00:07:42 -0300

    Adding methods eq and hash plus displaying path

--- Cell.py
+++ Cell.py
@@ -1,7 +1,7 @@
 class Cell():
             # A cell object knows about its location in the grid 
             # it also knows of its size with the variables x,y,w,h.
-            def __init__(self, tempX, tempY, tempW, tempH, food, obstacle, explored):
+            def __init__(self, tempX, tempY, tempW, tempH, food, obstacle, explored, path):
                 self.x = tempX
                 self.y = tempY
                 self.w = tempW
@@ -9,6 +9,7 @@ class Cell():
                 self.food = food
                 self.obstacle = obstacle
                 self.explored = 0
+                self.path = 0
                 
             def display(self):
                 #print(self.obstacle)
@@ -16,6 +17,10 @@ class Cell():
                     self.obstacle.display()
                 elif self.food != 0:
                     self.food.display()   
+                elif self.path != 0 and self.obstacle == 0:
+                    stroke(255)
+                    fill(0,255,0)
+                    rect(self.x,self.y, 20, 20)
                 elif self.explored != 0 and self.obstacle == 0:
                     stroke(255)
                     fill(255,255,0)
@@ -37,3 +42,15 @@ class Cell():
                     list.append(grid[self.w][self.h+1])   
                             
                 return list
+            
+            def __eq__(self, other):         
+                if not isinstance(other, Cell):
+                    print("not")
+                    return 0
+                if self.w == other.w and self.h == other.h:
+                    print("Self:", self.w, self.h, " Other:",other.w,other.h)
+                
+                return self.w == other.w and self.h == other.h
+            
+            def __hash__(self):
+                return hash(self.w) + hash(self.h)
